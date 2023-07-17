2b82d06da72c94676085a65f5c8c5935d59c5bd8
sadati <sadatiokeyo45@gmail.com>
2023-07-17 08:16:05 -0700

    display the numbers 0 to 9 separated by commas and spaces

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i;
+	int nums;
+	int nums1;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');
-    }
-
-    for (i = 0; i < 6; i++)
-    {
-        putchar(i + 'a');
-    }
-
-    putchar('\n');
-    return 0;
+	for (nums = '0'; nums <= '9'; nums++)
+	{
+	putchar(nums);
+	}
+	for (nums1 = 'a'; nums1 <= 'f'; nums1++)
+	{
+	putchar(nums1);
+	}
+	putchar('\n');
+	return (0);
 }
