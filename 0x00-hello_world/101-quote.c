3c2d0ba9ef489b99b11254613a1bfa0058b02a82
sadati <sadatiokeyo45@gmail.com>
2023-07-18 20:02:23 -0700

    prints _putchar, followed by a new line.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - print quote
  *
- * Description: Dora K's quote
+ * Description: prints Dors's quote
  *
  * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19, 59);
 	return (1);
 }
