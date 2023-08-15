2a3f6622ef94080f0170443ca39ac6fac69b1796
sadati <sadatiokeyo45@gmail.com>
2023-08-15 04:04:41 -0700

    define a macro named PI as an abbreviation for the token 3.1415926535

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,6 +1,6 @@
-#ifndef _OBJECT_LIKE_MACRO_H
-#define _OBJECT_LIKE_MACRO_H
+#ifndef _PI_
+#define _PI_
 
-#define SIZE 3.14159265359
+#define PI 3.14159265359
 
-#endif /*_OBJECT_LIKE_MACRO_H */
+#endif /*_PI_ */
