f6c6013c17b05e64f74aa64b8d6a57a328eaf7b4
sadati <sadatiokeyo45@gmail.com>
2023-08-18 06:38:40 -0700

    print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name- is a function to print the name of data type string
- * 
- * @name: name of a person
- * @f: is the function pointer to the name of the person
-*/
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: pointer to a function
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
